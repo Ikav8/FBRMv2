26ae50e52b1b4527e75c61a0b3c9eead8ca4e872
AOG <alex.ortiz.garcia@gmail.com>
2018-11-28 15:05:45 +0100

    pseudocodigo FBRMv2.py

--- FBRMv2.py
+++ FBRMv2.py
@@ -1,5 +1,5 @@
-
-# SUBMIT BUTTON : ctl00$BBuscar #
+import bs4
+import requests
 
 URL = "https://www.fbrm.org/competicion-327/competiciones-fbrm.aspx"
 
@@ -68,7 +68,37 @@ EQUIPOS = {
 
 
 def resultados_y_proximos_partidos_HTML():
-    pass
+
+    lista_resultados_partidos = []
+    lista_proximos_partidos = []
+
+    for equipo, equipo_form in EQUIPOS.items():
+        print('Comienza el web scraping para el equipo: ' + equipo)
+        r = requests.post(URL, equipo_form)
+        soup = bs4.BeautifulSoup(r.text, 'html.parser')
+        # Parse #
+
+    html = ['<xmp><table id="resultados_tabla"><tr><th>Equipo</th><th>Jornada</th><th>Equipo Local</th><th></th>' +
+            '<th></th><th>Equipo Visitante</th></tr>']
+
+    for resultado_partido in lista_resultados_partidos:
+        html.append(resultado_partido.to_html())
+
+    html.append('</table>')
+    html.append('<h2 style="text-align:center">Próximos Partidos</h2>')
+    html.append('<table id="resultados_tabla"><tr><th>Equipo</th><th>Equipo Local</th><th>Equipo Visitante</th>' +
+                '<th>Fecha</th><th>Lugar</th></tr>')
+
+    for proximo_partido in lista_proximos_partidos:
+        html.append(proximo_partido.to_html())
+
+    html.append('</table>')
+    html.append('<p style=\"text-align: center;\"><a target=\"_blank\" class=\"btn btn-theme-primary-outline\" ' +
+                'href=\"https://www.fbrm.org/competicion-309/competiciones-fbrm.aspx\">Más resultados y ' +
+                'clasificaciones</a></p>')
+    html.append('</xmp>')
+
+    return "\n".join(html)
 
 
 class ResultadoPartido():
